cb9f5793b0829861358bf1395e791282807ef5bb
Victor Monteiro <victor.vianna@gmail.com>
2020-07-25 15:32:11 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/vvmbrrj/MeuRemedio/MeuRemedioApplication.java
@@ -0,0 +1,13 @@
+package br.com.vvmbrrj.MeuRemedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuRemedioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuRemedioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vvmbrrj/MeuRemedio/MeuRemedioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vvmbrrj.MeuRemedio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuRemedioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
